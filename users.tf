4c8d8bfc79793f1c88a2b89dbd63929efbf01247
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-03-19 13:18:37 -0700

    Add tf_aws

--- users.tf
+++ users.tf
@@ -7,26 +7,6 @@ resource "aws_iam_access_key" "aleks" {
   user = aws_iam_user.aleks.name
 }
 
-resource "aws_iam_user" "tf_github" {
-  name = "tf_github"
-  tags = merge(local.common_tags)
-}
-
-resource "aws_iam_access_key" "tf_github" {
-  user = aws_iam_user.tf_github.name
-}
-
-
-resource "aws_iam_user" "tf_s3" {
-  name = "tf_s3"
-  tags = merge(local.common_tags)
-}
-
-resource "aws_iam_access_key" "tf_s3" {
-  user = aws_iam_user.tf_s3.name
-}
-
-
 resource "aws_iam_user" "twindb_test_runner" {
   name = "twindb_test_runner"
   tags = merge(local.common_tags)
