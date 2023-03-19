964abc2d314c42231ae5efca1ce1a9d692a67f1d
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-03-19 13:43:08 -0700

    Move tf admins to a module

--- terraform_admins.tf
+++ terraform_admins.tf
@@ -1,30 +1,12 @@
-resource "aws_iam_user" "tf_github" {
-  name = "tf_github"
-  tags = merge(local.common_tags)
+module "tf_admins" {
+  source = "./modules/tf_admin"
+  for_each = toset(
+    [
+      "tf_github",
+      "tf_aws",
+      "tf_s3"
+    ]
+  )
+  username = each.key
+  tags     = merge(local.common_tags)
 }
-
-resource "aws_iam_access_key" "tf_github" {
-  user = aws_iam_user.tf_github.name
-}
-
-
-resource "aws_iam_user" "tf_aws" {
-  name = "tf_aws"
-  tags = merge(local.common_tags)
-}
-
-resource "aws_iam_access_key" "tf_aws" {
-  user = aws_iam_user.tf_aws.name
-}
-
-
-resource "aws_iam_user" "tf_s3" {
-  name = "tf_s3"
-  tags = merge(local.common_tags)
-}
-
-resource "aws_iam_access_key" "tf_s3" {
-  user = aws_iam_user.tf_s3.name
-}
-
-## Terraform admins
