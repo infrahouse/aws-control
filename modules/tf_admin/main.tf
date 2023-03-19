319d5099a7d750a2c6a549418bc3e94d56a7f3fe
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-03-19 14:02:29 -0700

    Export IAM keys to secrents manager

--- modules/tf_admin/main.tf
+++ modules/tf_admin/main.tf
@@ -6,3 +6,19 @@ resource "aws_iam_user" "tf_admin" {
 resource "aws_iam_access_key" "tf_admin" {
   user = aws_iam_user.tf_admin.name
 }
+
+resource "aws_secretsmanager_secret" "tf_admin" {
+  name                    = "tf_admin/${var.username}"
+  recovery_window_in_days = 0
+  tags                    = var.tags
+}
+
+resource "aws_secretsmanager_secret_version" "version" {
+  secret_id = aws_secretsmanager_secret.tf_admin.id
+  secret_string = jsonencode(
+    {
+      "aws_access_key_id" : aws_iam_access_key.tf_admin.id
+      "aws_secret_access_key" : aws_iam_access_key.tf_admin.secret
+    }
+  )
+}
