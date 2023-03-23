b48338b4b8a389c9a4de8be77e5e8f8335579d03
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-03-22 17:54:36 -0700

    Move twindb user out

--- users.tf
+++ users.tf
@@ -6,17 +6,3 @@ resource "aws_iam_user" "aleks" {
 resource "aws_iam_access_key" "aleks" {
   user = aws_iam_user.aleks.name
 }
-
-resource "aws_iam_user" "twindb_test_runner" {
-  name = "twindb_test_runner"
-  tags = merge(local.common_tags)
-}
-
-resource "aws_iam_access_key" "twindb_test_runner" {
-  user = aws_iam_user.twindb_test_runner.name
-}
-
-resource "aws_iam_user_policy_attachment" "twindb_test_runner" {
-  policy_arn = aws_iam_policy.TwinDBTestRunner.arn
-  user       = aws_iam_user.twindb_test_runner.name
-}
