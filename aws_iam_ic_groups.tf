94ddf3a717f647bf3990febd59b936c1946e092f
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-10-24 08:47:01 -0700

    Import IC groups

---
+++ aws_iam_ic_groups.tf
@@ -0,0 +1,20 @@
+locals {
+  sso_groups = {
+    "AWSControlTowerAdmins" : "Admin rights to AWS Control Tower core and provisioned accounts"
+    "AWSLogArchiveAdmins" : "Admin rights to log archive account"
+    "AWSAccountFactory" : "Read-only access to account factory in AWS Service Catalog for end users"
+    "AWSServiceCatalogAdmins" : "Admin rights to account factory in AWS Service Catalog"
+    "AWSSecurityAuditPowerUsers" : "Power user access to all accounts for security audits"
+    "AWSAuditAccountAdmins" : "Admin rights to cross-account audit account"
+    "AWSSecurityAuditors" : "Read-only access to all accounts for security audits"
+    "AWSLogArchiveViewers" : "Read-only access to log archive account"
+  }
+
+}
+resource "aws_identitystore_group" "sso" {
+  for_each          = local.sso_groups
+  provider          = aws.aws-990466748045-uw1
+  display_name      = each.key
+  description       = each.value
+  identity_store_id = local.identity_store_id
+}
