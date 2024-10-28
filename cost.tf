dda77239d8cc35658c3f8d98c5a926ba7de62116
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-10-28 11:40:27 -0700

    move cost alert to us-east-1

--- cost.tf
+++ cost.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudwatch_metric_alarm" "daily0spend" {
+  provider            = aws.aws-990466748045-ue1
   alarm_name          = "daily_cost"
   namespace           = "AWS/Billing"
   metric_name         = "EstimatedCharges"
@@ -14,10 +15,12 @@ resource "aws_cloudwatch_metric_alarm" "daily0spend" {
 }
 
 resource "aws_sns_topic" "cost_notifications" {
+  provider    = aws.aws-990466748045-ue1
   name_prefix = "cost-daily-"
 }
 
 resource "aws_sns_topic_subscription" "cost_emails" {
+  provider  = aws.aws-990466748045-ue1
   endpoint  = "aleks@infrahouse.com"
   protocol  = "email"
   topic_arn = aws_sns_topic.cost_notifications.arn
