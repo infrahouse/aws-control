e326a766fbb059bf7eb5a5b519f137273047b60d
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-10-28 12:08:42 -0700

    Add provider config

--- providers.tf
+++ providers.tf
@@ -35,3 +35,16 @@ provider "aws" {
     }
   }
 }
+
+provider "aws" {
+  alias  = "aws-990466748045-ue1"
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::990466748045:role/ih-tf-aws-control-admin"
+  }
+  default_tags {
+    tags = {
+      "created_by" : "infrahouse/aws-control-990466748045" # GitHub repository that created a resource
+    }
+  }
+}
