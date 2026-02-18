215c941c28f1961649d89019f1a464141f93427a
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-02-18 11:07:22 -0800

    Remove AWS key references in outputs

--- backups.tf
+++ backups.tf
@@ -10,16 +10,6 @@ output "mediapc-bucket" {
   value = module.mediapc.bucket
 }
 
-output "mediapc-access-key" {
-  value     = module.mediapc.access_key
-  sensitive = true
-}
-
-output "mediapc-secret-key" {
-  value     = module.mediapc.secret_key
-  sensitive = true
-}
-
 output "mediapc-region" {
   value = module.mediapc.aws_region
 }
